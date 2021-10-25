e1257d52368a9e5f083fe8dc458c8ae204443988
Eagle <84711967+EAGLE1309@users.noreply.github.com>
2021-10-25 19:16:19 +0530

    Fixed nav bar error

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@
 const nav = document.getElementById("nav-links");
 const cross = document.getElementById("cross");
 const menu = document.getElementById("menu");
-
-cross.addEventListener("click", hide)
-
-function hide() {
-    nav.style.right = "-200px";
-};
+const navText = document.getElementById("nav-text");
 
 menu.addEventListener("click", show)
 function show() {
     nav.style.right = "5px";
+};
+
+cross.addEventListener("click", hide)
+navText.addEventListener("click", hide)
+
+function hide() {
+    nav.style.right = "-200px";
 };

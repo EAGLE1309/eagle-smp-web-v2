ea7edb51187c7be02286e378a0ea3846c1c823bb
Eagle <84711967+EAGLE1309@users.noreply.github.com>
2021-10-25 09:15:17 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//for menu bar
+
+var nav = document.getElementById("nav-links");
+
+        function hide() {
+            nav.style.right = "-200px";
+        }
+        function show() {
+            nav.style.right = "5";
+        }

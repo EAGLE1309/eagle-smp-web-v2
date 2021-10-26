8756993384aa79e0e4c787090d5ebcc36c507b1f
Eagle <84711967+EAGLE1309@users.noreply.github.com>
2021-10-26 09:33:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-//for menu bar
-
+const preloader = document.getElementById("preloader");
 const nav = document.getElementById("nav-links");
 const cross = document.getElementById("cross");
 const menu = document.getElementById("menu");
 const navText = document.getElementById("nav-text");
 
+// Preloader
+window.addEventListener("load", loader)
+function loader() {
+    preloader.style.display = "none";
+};
+
+//for menu bar
 menu.addEventListener("click", show)
 function show() {
     nav.style.right = "5px";
